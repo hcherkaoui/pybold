7a1c4f4fbf80dec0e59b54e07d83536b26452114
Hamza CHERKAOUI <hamza.cherkao@gmail.com>
2018-08-03 11:37:32 +0200

    [test] fix tests

--- pybold/tests/test_utils.py
+++ pybold/tests/test_utils.py
@@ -2,17 +2,17 @@
 """
 import unittest
 import numpy as np
-from pybold.utils import max_min_norm
+from pybold.utils import inf_norm
 
 
 class TestMinMaxNorm(unittest.TestCase):
-    def test_max_min_of_minmaxnorm(self):
+    def test_inf_norm(self):
         """ Test (max - min) of the min-max-normalized array is 1.0.
         """
         for N in [100, 128, 200, 256, 250, 300, 500, 600, 1000]:
             signal = np.random.randn(N)
-            signal = max_min_norm(signal)
-            np.testing.assert_almost_equal(signal.max() - signal.min(), 1.0)
+            signal = inf_norm(signal)
+            np.testing.assert_almost_equal(np.max(np.abs(signal)), 1.0)
 
 
 if __name__ == '__main__':
