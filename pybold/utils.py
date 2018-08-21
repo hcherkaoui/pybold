21ee59e94555d4bd9c57a3a95f7ea53f2d5bd9f0
Hamza CHERKAOUI <hamza.cherkao@gmail.com>
2018-08-21 15:36:27 +0200

    [enhanc] move grid_search fun

--- pybold/utils.py
+++ pybold/utils.py
@@ -1,9 +1,83 @@
 # coding: utf-8
 """ This module gathers usefull usefull functions.
 """
+import itertools
+import psutil
+from joblib import Parallel, delayed
 import numpy as np
 from numpy.linalg import norm as norm_2
 from scipy.interpolate import splrep, sproot
+
+
+def _default_wrapper(recons_func, **kwargs):
+    """ Default wrapper to parallelize the image reconstruction.
+
+    Parameters:
+    -----------
+    recons_func : func,
+        the function to run.
+
+    kwargs : dict,
+        the keywords args of 'func'.
+
+    Return:
+    -------
+    res : list,
+        the return of 'func'.
+
+    """
+    return recons_func(**kwargs)
+
+
+def grid_search(func, param_grid, wrapper=None, n_jobs=1, verbose=0):
+    """ Run `func` on the carthesian product of `param_grid`.
+
+    Parameters:
+    -----------
+    func : func,
+        function on which apply a grid-search.
+
+    param_grid : dict of list,
+        dict of params specify by list of values.
+
+    wrapper : fun (default=None),
+        optinal function that will be called with 'func' as first argument.
+
+    n_jobs : int (default=1),
+        number of CPU used.
+
+    verbose : int (default=0),
+        verbosity level.
+
+    Results:
+    --------
+    list_kwargs: dict,
+        the list of the params used for each reconstruction.
+    res: list,
+        the list of result for each reconstruction.
+    """
+    if wrapper is None:
+        wrapper = _default_wrapper
+    # sanitize value to list type
+    for key, value in param_grid.iteritems():
+        if not isinstance(value, list):
+            param_grid[key] = [value]
+    list_kwargs = [dict(zip(param_grid, x))
+                   for x in itertools.product(*param_grid.values())]
+    # Run the reconstruction
+    if verbose > 0:
+        if n_jobs == -1:
+            n_jobs_used = psutil.cpu_count()
+        elif n_jobs == -2:
+            n_jobs_used = psutil.cpu_count() - 1
+        else:
+            n_jobs_used = n_jobs
+        print(("Running grid_search for {0} candidates"
+               " on {1} jobs").format(len(list_kwargs), n_jobs_used))
+    res = Parallel(n_jobs=n_jobs, verbose=verbose)(
+                   delayed(wrapper)(func, **kwargs)
+                   for kwargs in list_kwargs)
+    return list_kwargs, res
 
 
 class Tracker:
